62610f5521f2c602f0c76b9174a3282d617aa8e4
APM <zsqwax102436@gmail.com>
2026-07-09 18:46:35 +0800

    Day01 - Python Basic

---
+++ day01/main.py
@@ -0,0 +1,18 @@
+print("==== AI Solution Consultant Bootcamp ====")
+name = input("請輸入你的名字: ")
+print(f"\n你好 {name}!")
+print(" 歡迎來到 AI Solution Consultant Bootcamp !")
+
+print("==== AI Career Profile ====")
+
+name = input("請輸入你的名字: ")
+age = input("請輸入你的年齡: ")
+goal = input("請輸入你的夢想工作: ")
+study = input("每天學習幾小時: ")
+
+print("\n====================")
+print(f"你好 : {name}!")
+print(f"年齡 : {age} 歲。")
+print(f"夢想工作 : {goal}")
+print(f"每天學習 : {study} 小時。")
+print("====================\n")
