2fd147e43c9b9eaf25a93491ce96be96626e84d3
APM <zsqwax102436@gmail.com>
2026-07-10 11:36:31 +0800

    Create main2.py

---
+++ Day02/main2.py
@@ -0,0 +1,14 @@
+print("=" * 20 )
+name = input("請輸入你的名字: ")
+age = int(input("請輸入你的年齡: "))
+height = int(input("請輸入你的身高: "))
+weight = int(input("請輸入你的體重: "))
+
+print()
+
+print("=" * 20 + " 個人資料 " + "=" * 20)
+print()
+print(f"姓名\t:{name}")
+print(f"年齡\t:{age}")   
+print(f"身高\t:{height} cm")
+print(f"體重\t:{weight} kg") 
