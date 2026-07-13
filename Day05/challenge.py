bad53c0d0abf83b8770098267c3358f201d52eb6
APM <zsqwax102436@gmail.com>
2026-07-13 22:50:45 +0800

    Create challenge.py

---
+++ Day05/challenge.py
@@ -0,0 +1,10 @@
+games =[]
+count = 1
+print("請輸入三個喜歡的遊戲")
+for i in range(3):
+    games.append(input(f"請輸入第{count}個喜歡的遊戲: "))
+    count += 1
+print("你最喜歡的遊戲")
+print()
+for game in games:
+    print(game)
